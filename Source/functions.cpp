9160ded236c8ba6634d51060f5dfe4e9343cd9cc
Juliusz99 <mateusz.m.bak@o2.pl>
2018-07-11 20:22:23 +0200

    no juz tu wpisywalem cos boze drogi

--- Source/functions.cpp
+++ Source/functions.cpp
@@ -1,11 +1,25 @@
 #include"functions.hpp"
 
 namespace functions
+
 {
 
 int fib(int n)
 {
-    return 0;
+    int nPlusTwoValue, tempOne, tempTwo;
+    int nValue = 0;
+    int nPlusOneValue = 1;
+
+    for ( int l = 1; l < n; l++ )
+    {
+        tempTwo = nValue;
+        tempOne = nPlusOneValue;
+        nPlusTwoValue = nPlusOneValue + nValue;
+        nPlusOneValue = nPlusTwoValue;
+        nValue = tempOne;
+    }
+
+    return nPlusTwoValue;
 }
 
 }
